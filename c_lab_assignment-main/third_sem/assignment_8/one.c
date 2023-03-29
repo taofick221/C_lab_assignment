d6c528a3b3a397d593879caf8398c03e523efe99
Md Taofick <107477685+taofick221@users.noreply.github.com>
2023-03-29 15:14:11 +0600

    Update one.c

--- c_lab_assignment-main/third_sem/assignment_8/one.c
+++ c_lab_assignment-main/third_sem/assignment_8/one.c
@@ -20,6 +20,8 @@ Q_NODE *add_node_last(Q_NODE *head , int data);
 Q_NODE *delete_first(Q_NODE *head);
 void print(Q_NODE *head);
 void option();
+// void option();
+
 
 int main() {
     Q_NODE *head = NULL;
